425288390a6406ea504119bb1f90479a4411a31f
Marian Ban <marian.ban@gmail.com>
2018-11-23 15:29:25 +0100

    component test

--- src/component.test.js
+++ src/component.test.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Simulate } from "react-dom/test-utils";
 import { Counter } from "./Counter";
 
 test("plain react test", () => {
-  var body = document.createElement("body");
-  ReactDOM.render(<Counter />, body);
+  var body = document.body;
+  const element = document.createElement("div");
+  const container = body.appendChild(element);
+  ReactDOM.render(<Counter />, container);
   const counter = body.querySelector("span");
+
   expect(counter.textContent).toBe("0");
   const inc = body.querySelector("button[data-testid='inc']");
-  console.log("windowddd", window);
-
+  /*
   inc.dispatchEvent(
     new window.MouseEvent("click", {
       bubbles: false,
@@ -17,6 +20,16 @@ test("plain react test", () => {
       view: window
     })
   );
+  */
+  Simulate.click(inc);
+
+  // ReactDOM.render(<Counter />, container);
+
   const counter2 = body.querySelector("span");
+
   expect(counter2.textContent).toBe("1");
+
+  document.body.removeChild(element);
+  console.log(document.body.innerHTML);
+  ReactDOM.unmountComponentAtNode(element);
 });
